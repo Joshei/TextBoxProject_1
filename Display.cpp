c910504424995b3ec57ffb7bbed22217ca8ed1e1
Joshua Eirman <joshuaeirm@gmail.com>
2017-10-07 16:05:08 -0400

    test

--- Display.cpp
+++ Display.cpp
@@ -5,7 +5,7 @@
 #include "windows.h"
 
 #include "Display.h"
-//changedfortesting - now, branch, final
+//changedfortesting - now, branch, final - 1
 using namespace std;
 
 	DISPLAY::DISPLAY(int display_width, int display_height) 
