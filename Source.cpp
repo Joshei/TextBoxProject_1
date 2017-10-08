5f78e4021c86cea770da18e9d85fdd9482157007
Joshua Eirman <joshuaeirm@gmail.com>
2017-10-08 15:29:22 -0400

    fixed

--- Source.cpp
+++ Source.cpp
@@ -64,7 +64,7 @@ int main(void) {
 
 		
 					   	
-		string test = "abcdefghij mnopq rs12345 6 7 8 9 45678";
+		string test = "abcd efghij mnopq rs12345 6 7 8 9 45678";
 					   							   //
 
 		while (one_time_loop == 0)
